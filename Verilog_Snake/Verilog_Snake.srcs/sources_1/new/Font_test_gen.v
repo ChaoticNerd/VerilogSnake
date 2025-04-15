9ffaaca5c8fb5ab0c846e1b520668f1d2f719d59
JustHiro <justin.narciso@gmail.com>
2025-04-15 15:48:33 -0700

    Font_Rom, Font_test_gen, and Font_top
    added Font_Rom, Font_test_gen, and Font_top, they are not fully done but just getting a concept in so that I can modify and fit to code, but I need to see what we have rn

---
+++ Verilog_Snake/Verilog_Snake.srcs/sources_1/new/Font_test_gen.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/15/2025 03:39:50 PM
+// Design Name: 
+// Module Name: Font_test_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module Font_test_gen
+   (
+    input wire clk,
+    input wire video_on,
+    input wire [9:0] pixel_x, pixel_y,
+    output reg [2:0] rgb_text
+   );
+
+   // signal declaration
+   wire [10:0] rom_addr;
+   wire [6:0] char_addr;
+   wire [3:0] row_addr;
+   wire [2:0] bit_addr;
+   wire [7:0] font_word;
+   wire font_bit, text_bit_on;
+
+   // body
+   // instantiate font ROM
+   Font_ROM font_unit
+      (.clk(clk), .addr(rom_addr), .data(font_word));
+   // font ROM interface
+   assign char_addr = {pixel_y[5:4], pixel_x[7:3]};
+   assign row_addr = pixel_y[3:0];
+   assign rom_addr = {char_addr, row_addr};
+   assign bit_addr = pixel_x[2:0];
+   assign font_bit = font_word[~bit_addr];
+   // "on" region limited to top-left corner
+   assign text_bit_on = (pixel_x[9:8]==0 && pixel_y[9:6]==0) ?
+                        font_bit : 1'b0;
+   // rgb multiplexing circuit
+   always @*
+      if (~video_on)
+         rgb_text = 3'b000; // blank
+      else
+         if (text_bit_on)
+            rgb_text = 3'b010;  // green
+          else
+            rgb_text = 3'b000;  // black
+
+endmodule
---
+++ Verilog_Snake/Verilog_Snake.srcs/sources_1/new/Font_test_top.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/15/2025 03:39:50 PM
+// Design Name: 
+// Module Name: Font_test_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Font_test_top(
+    input wire clk, reset,
+    output wire hsync, vsync,
+    output wire [2:0] rgb
+   );
+
+   // signal declaration
+   wire [9:0] pixel_x, pixel_y;
+   wire video_on, pixel_tick;
+   reg [2:0] rgb_reg;
+   wire [2:0] rgb_next;
+
+   // body
+   // instantiate vga sync circuit
+   vga_sync vsync_unit
+      (.clk(clk), .reset(reset), .hsync(hsync), .vsync(vsync),
+       .video_on(video_on), .p_tick(pixel_tick),
+       .pixel_x(pixel_x), .pixel_y(pixel_y));
+   // font generation circuit
+   Font_test_gen font_gen_unit
+      (.clk(clk), .video_on(video_on), .pixel_x(pixel_x),
+       .pixel_y(pixel_y), .rgb_text(rgb_next));
+   // rgb buffer
+   always @(posedge clk)
+      if (pixel_tick)
+         rgb_reg <= rgb_next;
+   // output
+   assign rgb = rgb_reg;
+
+endmodule
