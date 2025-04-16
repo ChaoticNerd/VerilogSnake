54b9a4da8d15f5fefda88d348817f9d634527f5c
Hanna Estrada <72177607+hanna-e326@users.noreply.github.com>
2025-04-16 09:53:07 -0700

    added file

---
+++ Verilog_Snake/Verilog_Snake.srcs/sources_1/new/snake_graph_animate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2025 07:20:24 PM
+// Design Name: 
+// Module Name: snake_graph_animate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module snake_graph_animate(
+
+    );
+// for ALL animations. use for snake and fruit
+
+
+endmodule
